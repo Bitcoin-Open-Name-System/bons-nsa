b19e3f801397767a851ba92b99412aaa3731ff20
Jinhyeok Heo <hjh4790@knou.ac.kr>
2025-06-29 00:05:22 +0900

    feat(getNetworkParams): implement network selection logic with passing tests

--- pkg/genp2tr/address.go
+++ pkg/genp2tr/address.go
@@ -1,25 +1,20 @@
 package genp2tr
 
 import (
-	// "fmt"
+	"fmt"
 
 	"github.com/btcsuite/btcd/chaincfg"
 )
 
 func getNetworkParams(network Network) (*chaincfg.Params, error) {
-	// TODO :
-
-	// switch network {
-	// case Mainnet:
-	// 	return &chaincfg.MainNetParams, nil
-	// case Testnet:
-	// 	return &chaincfg.TestNet3Params, nil
-	// case Regtest:
-	// 	return &chaincfg.RegressionNetParams, nil
-	// default:
-	// 	return nil, fmt.Errorf("invalid network: %s", network)
-	// }
-
-	// TODO :
-	return &chaincfg.MainNetParams, nil
+	switch network {
+	case Mainnet:
+		return &chaincfg.MainNetParams, nil
+	case Testnet:
+		return &chaincfg.TestNet3Params, nil
+	case Regtest:
+		return &chaincfg.RegressionNetParams, nil
+	default:
+		return nil, fmt.Errorf("invalid network: %s", network)
+	}
 }
