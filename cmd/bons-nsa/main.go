4a1e9a0d5f69f5bdc83216c7af2f0b710be35077
Jinhyeok Heo <hjh4790@knou.ac.kr>
2025-07-01 00:38:46 +0900

    feat: add CLI logic to generate p2tr burn address

--- cmd/bons-nsa/main.go
+++ cmd/bons-nsa/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/Bitcoin-Open-Name-System/bons-nsa/pkg/genp2tr"
 )
 
 func main() {
-	fmt.Println("TODO: ")
+	seed := genp2tr.PrimarySeed
+	net := genp2tr.Regtest
+
+	netParams, err := genp2tr.GetNetworkParams(net)
+	if err != nil {
+		log.Fatalf("Invalid network: %v", err)
+	}
+
+	address, internalPubKey, outputPubkey, err := genp2tr.CreateTaprootBurnAddress(seed, netParams)
+	if err != nil {
+		log.Fatalf("Failed to create burn address: %v", err)
+	}
+
+	fmt.Printf("================= | Successfully created P2TR burn address | =================\n\n")
+	fmt.Printf("Burn Address: 	%s\n\n", address)
+	fmt.Printf("Input Seed: 	%s\n", seed)
+	fmt.Printf("Network: 	%s\n", net)
+	fmt.Printf("Internal PubKey:%s\n", internalPubKey)
+	fmt.Printf("Output PubKey: 	%s\n", outputPubkey)
 }
