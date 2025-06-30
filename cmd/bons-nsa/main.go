11d917d4e8d4e68436afd98bcdf9f91ce92ea81f
Jinhyeok Heo <hjh4790@knou.ac.kr>
2025-07-01 01:09:00 +0900

    feat(cmd): add 'create' subcmd with '-s' and '-n' flags
    create has 2 subflags
    -s : seed for generate p2tr address
    -n : network (mainnet, testnet, regtest)

--- cmd/bons-nsa/main.go
+++ cmd/bons-nsa/main.go
@@ -1,30 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Bitcoin-Open-Name-System/bons-nsa/pkg/genp2tr"
 )
 
 func main() {
-	seed := genp2tr.PrimarySeed
-	net := genp2tr.Regtest
+	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
+	seed := createCmd.String("s", genp2tr.PrimarySeed, "seed string for address generation")
+	net := createCmd.String("n", string(genp2tr.Mainnet), "network to use (mainnet, testnet, regtest)")
 
-	netParams, err := genp2tr.GetNetworkParams(net)
-	if err != nil {
-		log.Fatalf("Invalid network: %v", err)
+	if len(os.Args) < 2 {
+		fmt.Printf("asdfasdf\n")
+		os.Exit(1)
 	}
 
-	address, internalPubKey, outputPubkey, err := genp2tr.CreateTaprootBurnAddress(seed, netParams)
-	if err != nil {
-		log.Fatalf("Failed to create burn address: %v", err)
-	}
+	switch os.Args[1] {
+	case "create":
+		createCmd.Parse(os.Args[2:])
 
-	fmt.Printf("================= | Successfully created P2TR burn address | =================\n\n")
-	fmt.Printf("Burn Address: 	%s\n\n", address)
-	fmt.Printf("Input Seed: 	%s\n", seed)
-	fmt.Printf("Network: 	%s\n", net)
-	fmt.Printf("Internal PubKey:%s\n", internalPubKey)
-	fmt.Printf("Output PubKey: 	%s\n", outputPubkey)
+		netParams, err := genp2tr.GetNetworkParams(genp2tr.Network(*net))
+		if err != nil {
+			log.Fatalf("Invalid network: %v\n", err)
+		}
+
+		address, internalPubKey, outputPubkey, err := genp2tr.CreateTaprootBurnAddress(*seed, netParams)
+		if err != nil {
+			log.Fatalf("Failed to create burn address: %v\n", err)
+		}
+
+		fmt.Printf("================= | Successfully created P2TR burn address | =================\n\n")
+		fmt.Printf("Burn Address	: 	%s\n\n", address)
+		fmt.Printf("Input Seed	: 	%s\n", *seed)
+		fmt.Printf("Network		: 	%s\n", *net)
+		fmt.Printf("Internal PubKey	:	%s\n", internalPubKey)
+		fmt.Printf("Output PubKey	: 	%s\n", outputPubkey)
+	default:
+		fmt.Printf("command %s not found\n", os.Args[1])
+		os.Exit(1)
+	}
 }
